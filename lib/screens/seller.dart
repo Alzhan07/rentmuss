f1347a9b1ada138d79b09958109eb37a90068e76
Alzhan07 <yariisstolineed@gmail.com>
2025-10-22 14:12:56 +0500

    м

--- lib/screens/seller.dart
+++ lib/screens/seller.dart
@@ -1,4 +1,28 @@
-class Seller {}
-Мао Мао
-ебать нахуй
-этика кайда нурис
+class Seller {
+  String? id;
+  String? name;
+  String? email;
+  String? phone;
+
+  Seller({this.id, this.name, this.email, this.phone});
+
+  // Factory method to create a Seller from JSON
+  factory Seller.fromJson(Map<String, dynamic> json) {
+    return Seller(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone'],
+    );
+  }
+
+  // Method to convert a Seller to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+    };
+  }
+}
